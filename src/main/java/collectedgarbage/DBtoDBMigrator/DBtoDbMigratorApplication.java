05170df7ee4e1496800c05b0fbcc724354244bab
Na <resurrection04@gmail.com>
2023-06-19 22:38:09 +0900

    first commit

---
+++ src/main/java/collectedgarbage/DBtoDBMigrator/DBtoDbMigratorApplication.java
@@ -0,0 +1,13 @@
+package collectedgarbage.DBtoDBMigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBtoDbMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBtoDbMigratorApplication.class, args);
+	}
+
+}
